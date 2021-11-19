4901fc598c0a7aac214244855d2560d23c2d5484
Bryan Restine <brlabelectronics@gmail.com>
2021-11-19 11:47:14 -0600

    -Added current time and day function and print them to the terminal for debugging purposes

--- relayshieldargon.ino
+++ relayshieldargon.ino
@@ -52,9 +52,11 @@ int custom;
 
 // float assignemnts
 float voltage = 0;
-// other variables
-unsigned long lastmillis = 0;
 
+// Timing variables
+unsigned long batterylastmillis = 0;
+unsigned long currentTimelastmillis = 0;
+unsigned long currentDaylastmillis = 0;
 
 BlynkTimer timer;
 
@@ -131,20 +133,25 @@ BLYNK_WRITE(V6)
 {
     if(param.asInt() == 1)
     {
-    
+    alldays = 1;
+    weekdays = 0;
+    weekend = 0;
+    custom = 0;
+    Blynk.syncVirtual(V10);
     }
          else
         {
-                
+        alldays = 0; 
         }
 
 }
 
 
-BLYNK_WRITE(V10) {
+BLYNK_WRITE(V10) { // alldays schedule
+  TimeInputParam t(param);
+  
   if(alldays == 1)
   {
-  TimeInputParam t(param);
 
   // Process start time
 
@@ -206,12 +213,69 @@ BLYNK_WRITE(V10) {
   }
 }
 
-void batteryV(){
+void batteryV(){ 
+  // custom battery function
     double voltage = analogRead(BATT)*0.0011224;
     String BV = String(voltage,2) + String("V");
     Particle.publish("Battery Voltage", BV, PRIVATE);
     int SoC = map(voltage, 3.20, 4.14, 0.00, 100.00);
     Blynk.virtualWrite(V4, SoC);
+}
+
+void currentTime(){ 
+  // function to get current local time for debugging 
+  int timezoneoffset = -6;
+  Time.zone(timezoneoffset);
+  String currentTime = String("Current Time: ") + String(Time.hourFormat12()) + ":" + String(Time.minute());
+  terminal.print(currentTime); // current time in hours and minutes
+  terminal.flush();
+  int isAM = Time.isAM();
+  if(isAM == 1){
+    terminal.println("AM");
+    terminal.flush();
+  }
+  else{
+    terminal.println("PM");
+    terminal.flush();
+  }
+  if(alldays == 1)
+  {
+  }
+}
+void currentDay(){ 
+  // function to get current local day for debugging
+  int currentweekday = Time.weekday();
+  switch (currentweekday)
+  {
+    case 1:
+    terminal.println("Sunday");
+    terminal.flush();
+    break;
+    case 2:
+    terminal.println("Monday");
+    terminal.flush();
+    break;
+   case 3: 
+    terminal.println("Tuesday");
+    terminal.flush();
+    break;
+    case 4:
+    terminal.println("Wednesday");
+    terminal.flush();
+    break;
+    case 5:
+    terminal.println("Thursday");
+    terminal.flush();
+    break;
+    case 6:
+    terminal.println("Friday");
+    terminal.flush();
+    break;
+    case 7:
+    terminal.println("Saturday");
+    terminal.flush();
+    break;
+  }
 }
 
 void setup() {
@@ -230,18 +294,32 @@ void setup() {
   // This will print Blynk Software version to the Terminal Widget when
   // your hardware gets connected to Blynk Server
   terminal.println(F("Blynk v" BLYNK_VERSION ": Device has Booted"));
-  terminal.println(F("------------------------"));
+  currentTime();
+  currentDay();
   terminal.flush();
 }
 
 void loop() {
  Blynk.run();
   timer.run();
-  // update in 2 sec intervals
-            if ((millis() - lastmillis) > 120000) {
-                lastmillis = millis();
-                batteryV();
-            }
+  // update in 120 sec intervals
+  if ((millis() - batterylastmillis) > 120000) 
+  {
+      batterylastmillis = millis();
+      batteryV();
+  }
+  // update in 60 sec intervals
+  if ((millis() - currentTimelastmillis) > 60000) 
+  {
+      currentTimelastmillis = millis();
+      currentTime();
+  }
+  // update in 30 min intervals
+  if ((millis() - currentDaylastmillis) > 1800000) 
+  {
+      currentDaylastmillis = millis();
+      currentDay();
+  }
 
   // You can inject your own code or combine it with other sketches.
   // Check other examples on how to communicate with Blynk. Remember
